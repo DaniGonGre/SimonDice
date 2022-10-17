ae08e623f4734ea15c21224e4c71541e341ee5ce
DaniGonGre <dgonzalezgregores@danielcastelao.org>
2022-10-17 10:44:59 +0200

    creacion de variables

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -3,10 +3,10 @@ package com.example.simondice
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    //Datos
-    var secuencia = arrayListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,7 +14,52 @@ class MainActivity : AppCompatActivity() {
 
         //Variables
         var random = (0..3).random()
+        var record = 0
+        var velocidad = 0
+        var ronda = 0
+        var secuencia = arrayListOf<String>()
+        secuencia.add("verde")
+        secuencia.add("azul")
+        secuencia.add("amarillo")
+        secuencia.add("rojo")
+
         val colores = arrayOf("verde", "amarillo", "azul", "rojo")
+
+        fun iniciaPartida() {
+            Toast.makeText(this, "Memoriza la secuencia", Toast.LENGTH_LONG).show()
+        }
+
+        fun mostrarRonda() {
+            val ronda: TextView = findViewById(R.id.ronda)
+            ronda.text = ("Rondas: " + ronda)
+        }
+
+        fun bRojo(rojo: Button) {
+            rojo.setBackgroundColor(resources.getColor(R.color.white))
+            rojo.setBackgroundColor(resources.getColor(R.color.rojo))
+        }
+        fun bAmarillo(amarillo: Button) {
+            amarillo.setBackgroundColor(resources.getColor(R.color.white))
+            amarillo.setBackgroundColor(resources.getColor(R.color.amarillo))
+        }
+        fun bVerde(verde: Button) {
+            verde.setBackgroundColor(resources.getColor(R.color.white))
+            verde.setBackgroundColor(resources.getColor(R.color.verde))
+        }
+        fun bAzul(azul: Button) {
+            azul.setBackgroundColor(resources.getColor(R.color.white))
+            azul.setBackgroundColor(resources.getColor(R.color.azul))
+        }
+
+
+
+
+
+
+
+
+
+
 
     }
 }
