aafbfba64620d86ae7b33fa204a1f44737a506b4
DaniGonGre <dgonzalezgregores@danielcastelao.org>
2022-11-10 11:52:43 +0100

    modificacion comments MyViewModel.kt

--- app/src/main/java/com/example/simondice/MyViewModel.kt
+++ app/src/main/java/com/example/simondice/MyViewModel.kt
@@ -10,32 +10,30 @@ import kotlin.random.Random
  */
 class MyViewModel() : ViewModel() {
 
-    // para que sea mas facil la etiqueta del log
+    // Creamos una constante privada para darle cambiarle la etiqueta al log
     private val TAG_LOG: String = "mensaje ViewModel"
 
-    // este va a ser nuestra lista para la secuencia random
-    // usamos mutable, ya que la queremos modificar
+    // Este va a ser nuestra lista para la secuencia random, usamos mutable para modificar
     val numbers = mutableListOf<Int>()
     // definimos una MutableLiveData
     // para poder observar los valores de la MutableList<Int>
     val livedata_numbers = MutableLiveData<MutableList<Int>>()
 
-    // inicializamos variables cuando instanciamos
+    // Inicializamos variables cuando instanciamos
     init {
         Log.d(TAG_LOG, "Inicializamos livedata")
         livedata_numbers.value = numbers
     }
 
     /**
-     * añadimos entero random al
+     * Función que añade un valor random a la lista
      */
     fun sumarRandom() {
-        // añadimos entero random a la lista
+        // Añadimos entero random a la lista
         numbers.add(Random.nextInt(0,4))
-        // actualizamos el livedata, de esta manera si hay un observador
-        // este recibirá la nueva lista
+        // Actualizamos el livedata, de esta manera si hay un observador, este recibirá la nueva lista
         livedata_numbers.setValue(numbers)
-        // la mostramos en el logcat
+        // La mostramos en el logcat
         Log.d(TAG_LOG, "Añadimos Array al livedata:" + numbers.toString())
     }
 }
